988e86e4cdfa382d8b0b2d6670d5ca491af2a59a
jayzer0098 <jayzer0098@gmail.com>
2015-08-11 09:09:26 +0800

    Illuminate no homepage yet

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Hash;
+use Validator;
+use Input;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+   return view('index');
+}
